1b3f6f618e1546862307a977ddeafa8a1894f236
jiji3000 <tokyo.orinpic@gmail.com>
2016-05-07 19:50:34 +0900

    TabLayoutを適応したViewPagerサンプル

---
+++ app/src/main/java/com/example/jiji30000/viewpagersample/SampleFragmentPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.jiji30000.viewpagersample;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SampleFragmentPagerAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[]{"Tab1", "Tab2", "Tab3"};
+    private Context context;
+
+    public SampleFragmentPagerAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        // fragmentを切り替える
+        switch(position){
+            case 0:
+                return new PageFragment();
+            case 1:
+                return new PageFragment2();
+            case 2:
+                return new PageFragment3();
+        }
+        return null;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
