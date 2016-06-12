8642bd3af023589fe6302d26547ee5b2f2bb4885
jiji3000 <tokyo.orinpic@gmail.com>
2016-06-12 17:38:34 +0900

    loopviewpagerを追加

--- app/src/main/java/com/example/jiji30000/viewpagersample/PageFragment.java
+++ app/src/main/java/com/example/jiji30000/viewpagersample/PageFragment.java
@@ -1,34 +1,84 @@
 package com.example.jiji30000.viewpagersample;
 
 import android.os.Bundle;
-import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class PageFragment extends Fragment {
     private static final String TAG = PageFragment.class.getName();
     // スライド用の部品
-    private ViewPager mPager;
+    private LoopViewPager mPager;
+    private int mMargin;
+    private LinearLayout mIndicatorLayout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         Log.d(TAG, "onCreateView");
         View view = inflater.inflate(R.layout.viewpager_fragment, container, false);
-        mPager = (ViewPager)view.findViewById(R.id.viewpager);
-        mPager.setAdapter(new SampleFragmentPagerAdapter(getChildFragmentManager(),
-                getContext()));
+        mPager = (LoopViewPager) view.findViewById(R.id.viewpager);
+        mPager.setAdapter(new SamplePagerAdapter(getContext()));
+        mPager.setCurrentItem(0);
+        mPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-        // 上部にタブをセットする
-        TabLayout tabLayout = (TabLayout) view.findViewById(R.id.tabs);
-        tabLayout.setupWithViewPager(mPager);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                Log.d(TAG, "onPageSelected = " + position);
+                // indicator更新
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                layoutParams.setMargins(mMargin, 0, 0, 0);
+                mIndicatorLayout.removeAllViews();
+                for (int i = 0; i < SamplePagerAdapter.PAGE_COUNT; ++i) {
+                    if (position == i) {
+                        View indicatorOn = getActivity().getLayoutInflater().inflate(R.layout.indicator, null);
+                        indicatorOn.setLayoutParams(layoutParams);
+                        mIndicatorLayout.addView(indicatorOn, layoutParams);
+                    } else {
+                        View indicatorOff = getActivity().getLayoutInflater().inflate(R.layout.indicator_off, null);
+                        indicatorOff.setLayoutParams(layoutParams);
+                        mIndicatorLayout.addView(indicatorOff);
+                    }
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        // indicator
+        mIndicatorLayout = (LinearLayout) view.findViewById(R.id.indicators);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        DisplayMetrics metrics = getResources().getDisplayMetrics();
+        mMargin = (int) (metrics.density * 4);
+        layoutParams.setMargins(mMargin, 0, 0, 0);
+        for (int i = 0; i < SamplePagerAdapter.PAGE_COUNT; ++i) {
+            if (i == 0) {
+                View indicatorOn = getActivity().getLayoutInflater().inflate(R.layout.indicator, null);
+                indicatorOn.setLayoutParams(layoutParams);
+                mIndicatorLayout.addView(indicatorOn, layoutParams);
+            } else {
+                View indicatorOff = getActivity().getLayoutInflater().inflate(R.layout.indicator_off, null);
+                indicatorOff.setLayoutParams(layoutParams);
+                mIndicatorLayout.addView(indicatorOff);
+            }
+        }
         return view;
     }
 }
---
+++ app/src/main/java/com/example/jiji30000/viewpagersample/SamplePagerAdapter.java
@@ -0,0 +1,49 @@
+package com.example.jiji30000.viewpagersample;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.*;
+import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SamplePagerAdapter extends PagerAdapter{
+
+    public static final int PAGE_COUNT = 3;
+    private Context mContext;
+    private static final int RES_IDS[] = new int[]{R.drawable.img1,R.drawable.img2,R.drawable.img3};
+
+    SamplePagerAdapter(Context context){
+        mContext = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup collection, int position, Object view) {
+        //ViewPagerに登録していたTextViewを削除する
+        collection.removeView((View)view);
+    }
+    @Override
+    public Object instantiateItem(ViewGroup container,int position){
+        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(mContext.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.content_fragment_page,null);
+        ImageView imageView = (ImageView)view.findViewById(R.id.main_image);
+        imageView.setImageResource(RES_IDS[position]);
+        container.addView(view);
+        return view;
+    }
+}
